213397cae7d5b5d09927636d411abf497d07ff3e
amandabledsoe <amandajbledsoe@gmail.com>
2022-04-19 22:04:24 -0400

    MVP. Need to work on all of the bonus requirements next

--- Lab4_Capstone_PigLatin/Program.cs
+++ Lab4_Capstone_PigLatin/Program.cs
@@ -8,26 +8,39 @@ namespace Lab4_Capstone_PigLatin
         static void Main(string[] args)
         {
             char[] vowels = new char[] {'a', 'e', 'i', 'o', 'u', 'y'};
+            char[] alphabetLetters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] separators = new char[] { ' ', '.' };
             bool doingLab4 = true;
+
+            Console.WriteLine("Welcome to the Pig Latin phrase generator!");
+            Console.WriteLine("");
             while(doingLab4)
             {
                 Console.WriteLine("Enter a word and I will translate it into Pig Latin for you!");
-                Console.Write("Your Word: ");
-                string userWordString = Console.ReadLine().ToLower();
                 Console.WriteLine("");
+                Console.Write("Your phrase: ");
+                string userWordString = Console.ReadLine().ToLower();
+                string[] subs = userWordString.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 
-                if(userWordString.IndexOfAny(vowels) == 0)
+                Console.Write("Pig Latin phrase: ");
+
+                foreach (var sub in subs)
                 {
-                    Console.WriteLine($"Pig Latin: {userWordString}way");
-                    Console.WriteLine("");
+                    if (sub.StartsWith('a') || sub.StartsWith('e') || sub.StartsWith('i') || sub.StartsWith('o') || sub.StartsWith('u') || sub.StartsWith('y'))
+                    {
+                        string splitFrontOfWord = sub.Substring(0, sub.IndexOfAny(vowels));
+                        string newShortString = sub.Remove(0, sub.IndexOfAny(vowels));
+                        Console.Write($"{newShortString}{splitFrontOfWord}way ");
+                    }
+                    else
+                    {
+                        string splitFrontOfWord = sub.Substring(0, sub.IndexOfAny(vowels));
+                        string newShortString = sub.Remove(0, sub.IndexOfAny(vowels));
+                        Console.Write($"{newShortString}{splitFrontOfWord}ay ");
+                    }
                 }
-                else
-                {
-                    // this is not right and I need to figure out how to split the words starting at the first vowel
-                    Console.WriteLine("");
-                    Console.WriteLine($"Pig Latin: {userWordString}ay");
-                    Console.WriteLine("");
-                }
+                Console.WriteLine("");
+                Console.WriteLine("");
                 doingLab4 = RunProgramAgainPrompt();
             }
             Console.WriteLine("Thanks for using the Pig Latin program!");
@@ -38,7 +51,7 @@ namespace Lab4_Capstone_PigLatin
             bool promptingUser = true;
             while (promptingUser)
             {
-                Console.WriteLine("Would you like to translate the pig latin for another word?");
+                Console.WriteLine("Would you like to translate the pig latin for another phrase?");
                 Console.WriteLine("Enter 'y' or 'yes' to do another exercise, or enter 'n' or 'no' to exit the program.");
                 string userInput = Console.ReadLine().ToLower();
                 if (userInput == "y" || userInput == "yes")
